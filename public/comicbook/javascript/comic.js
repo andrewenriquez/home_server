213017e96e8a587c46915a86b869a77d4114e119
andrewenriquez <andrewenriquez@email.arizona.edu>
2020-10-19 22:29:46 -0700

    starting comic book server

---
+++ public/comicbook/javascript/comic.js
@@ -0,0 +1,14 @@
+  
+  function testButtonFunction() {
+    console.log("testButton works");
+  }
+  
+  
+  
+  // Handle authentication on page load
+  $(function() {
+    // If there's no authToekn stored, redirect user to 
+    
+    // Register event listeners
+    $("#testbutton").click(testButtonFunction);
+  });

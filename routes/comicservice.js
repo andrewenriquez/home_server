4453676745228292c1bf661fa54000d5203d3387
andrewenriquez <andrewenriquez@email.arizona.edu>
2020-10-20 09:36:34 -0700

    added xml2json and decompress

--- routes/comicservice.js
+++ routes/comicservice.js
@@ -1,6 +1,12 @@
 var express = require('express');
+fs = require('fs');
+xml2js = require('xml2js');
+const decompress = require('decompress');
+
+
 var router = express.Router();
 var { Todos } = require('../models/todos');
+var parser = new xml2js.Parser();
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   const todoss =  await Todos.find();
@@ -8,4 +14,14 @@ router.get('/', async function(req, res, next) {
   res.render('index', { title: 'Todos:', todos: todoss });
 });
 
+router.get('/xml2js', async function(req, res, next) {
+  console.log("/xml2js router working");
+
+});
+
+router.get('/decompress', async function(req, res, next) {
+  console.log("/decompress router working");
+
+});
 module.exports = router;
+
